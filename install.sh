58bf238b28797fd4d11b004f84d7a1033397bbca
qsdrqs <qsdrqs@gmail.com>
2020-03-10 02:56:47 +0800

    initial commit

---
+++ install.sh
@@ -0,0 +1,52 @@
+#check root
+if  [ `id -u` != '0' ] ;then
+    echo "Please use root privilege to make sure installation sucessful."
+    exit
+fi
+
+#custom directory
+if [ "$1" != "" ] && [ "$1" != "-s" ];then
+    echo "use '-s [service_dir]' to assign a customized systemd directory."
+    exit
+elif [ "$1" == "-s" ] && [ !"$2" ];then
+    echo "use '-s [service_dir]' to assign a customized systemd directory."
+    exit
+elif [ "$1" == "-s" ] && [ "$2" ];then
+    dir=$2
+elif [ !"$1" ];then
+    dir=/usr/lib/systemd/system/
+else
+    echo "Some errors occured..."
+    exit
+fi
+
+echo "Be sure you are under root privilege have installed the following dependences:"
+echo "  1.cmake"
+echo "  2.libevdev"
+echo "  3.yaml-cpp"
+read -p "Have you installed all?(Y/n)" option
+if [ $option == "n" ] || [ $option == "N" ];then
+    echo "Aborting installation"
+    exit
+fi
+#install tools and caps2esc
+cd ./tools && cmake
+cd ./build && make
+make install
+cd ../../
+cd ./caps2esc && cmake
+cd ./build && make
+make install 
+cd ../../
+mv ./udevmon.yaml /etc/udevmon.yaml
+#May not work if local service files is not set here
+function move_service {
+    mv ./udevmon.service     sudo systemctl enable --now udevmon
+}
+if move_service ;then
+    echo "Service has been installed and started successfully, use 'systemctl disable --now' to stop the service."
+elif [ !$1 ]
+    echo "Systemd services is not in '/usr/lib/systemd/system/', please use -s [service_dir] to assign your customized location"
+else
+    echo "You may not input a correct directory."
+fi
